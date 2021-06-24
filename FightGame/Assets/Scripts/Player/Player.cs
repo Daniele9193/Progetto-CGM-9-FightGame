865fad50083d040afefdabed411b8484fc095a5f
Francesco <30899725+Franz95@users.noreply.github.com>
2021-06-24 17:38:45 +0200

    Lavoro Animazioni
    Corrette e collegate le animazioni avanti e indietro; inserite le animazioni di Knocdown, getup, win e secondo idle (da far partire sotto un tot di vita).
    Modifiche script per non far muovere se IsDead o IsKnocked sono veri ma sono ancora da collegare con script esterno.

--- FightGame/Assets/Scripts/Player/Player.cs
+++ FightGame/Assets/Scripts/Player/Player.cs
@@ -5,11 +5,14 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 0.1f;
+    [SerializeField] private float _speed = 0.005f;
     private Vector2 _inputMovement;
     [SerializeField] private InputControl _inputControl;
     private Animator _anim;
-    private bool movimento = false;
+    private bool movimentoavanti = false;
+    private bool movimentoindietro = false;
+    private bool isDead = false;
+    private bool isWin = false;
 
     private void Start()
     {
@@ -26,22 +29,26 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Animazione();
-        transform.position += new Vector3(_inputMovement.x * _speed * Time.deltaTime, 0.0f, 0.0f);
+        if (_anim.GetBool("IsDead") == false && _anim.GetBool("IsKnocked") == false)
+        {
+            transform.position += new Vector3(_inputMovement.x * _speed * Time.deltaTime, 0.0f, 0.0f);
+        }
     }
 
     private void OnMovement(InputAction.CallbackContext value)
     {
-        Debug.Log(value.control.displayName);
-        //movimento = true;
         _inputMovement = value.ReadValue<Vector2>();
-
-        //_anim.SetTrigger("MoveForward");
-        Debug.Log(_inputMovement);
+        if (_anim.GetBool("IsKnocked") == true)
+        {
+            _anim.SetBool("IsKnocked", false);
+        }
     }
 
     private void Animazione()
     {
-        movimento = (_inputMovement.x < -0.1f || _inputMovement.x > 0.1f || _inputMovement.y < -0.1f ||
-                     _inputMovement.y > 0.1f) ? true: false;
+        movimentoavanti = (_inputMovement.x > 0.1f) ? true: false;
+        _anim.SetBool("MoveForward", movimentoavanti);
+        movimentoindietro = (_inputMovement.x < -0.1f) ? true: false;
+        _anim.SetBool("MoveBackward", movimentoindietro);
     }
 }

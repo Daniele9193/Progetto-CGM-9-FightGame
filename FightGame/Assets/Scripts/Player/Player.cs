562064e7ea2009ace97f9332961f37d482ec4781
Francesco <30899725+Franz95@users.noreply.github.com>
2021-06-29 13:08:43 +0200

    Animazioni Pugno, Calcio e Parata Normale
    Aggiunta animazioni pugno, calcio e parata normale all'animator nell'Idle da Sanoò e loro implementazione in codice.

--- FightGame/Assets/Scripts/Player/Player.cs
+++ FightGame/Assets/Scripts/Player/Player.cs
@@ -11,39 +11,65 @@ public class Player : MonoBehaviour
     private Animator _anim;
     private bool movimentoavanti = false;
     private bool movimentoindietro = false;
-    private bool isDead = false;
-    private bool isWin = false;
+    //private bool blocco = false;
 
     private void Start()
     {
         _anim = GetComponent<Animator>();
     }
-    
-    void OnEnable()
+
+    private void Awake()
     {
-        _inputControl.FighterControl.Move.performed += OnMovement;
-        _inputControl.FighterControl.Move.Enable();
+        _inputControl = new InputControl();
     }
     
+    private void OnEnable()
+    {
+        _inputControl.FighterControl.Move.performed += OnMovement;
+        _inputControl.FighterControl.Kick.performed += KickLeft;
+        _inputControl.FighterControl.Punch.performed += RightPunch;
+        _inputControl.FighterControl.Block.performed += IsBlocking;
+        _inputControl.FighterControl.Block.canceled += IsBlocking;
+        _inputControl.FighterControl.Move.Enable();
+        _inputControl.FighterControl.Kick.Enable();
+        _inputControl.FighterControl.Punch.Enable();
+        _inputControl.FighterControl.Block.Enable();
+    }
 
     private void Update()
     {
         Animazione();
-        if (_anim.GetBool("IsDead") == false && _anim.GetBool("IsKnocked") == false)
+
+        if (_anim.GetBool("IsDead") == false && _anim.GetBool("IsKnocked") == false && _anim.GetBool("IsBlocking")==false)
         {
             transform.position += new Vector3(_inputMovement.x * _speed * Time.deltaTime, 0.0f, 0.0f);
         }
     }
 
-    private void OnMovement(InputAction.CallbackContext value)
+    public void OnMovement(InputAction.CallbackContext value)
     {
         _inputMovement = value.ReadValue<Vector2>();
         if (_anim.GetBool("IsKnocked") == true)
         {
-            _anim.SetBool("IsKnocked", false);
+           _anim.SetBool("IsKnocked", false);
         }
     }
 
+    public void KickLeft(InputAction.CallbackContext value)
+    {
+        _anim.SetTrigger("LeftKick");
+    }
+
+    public void RightPunch(InputAction.CallbackContext value)
+    {
+        _anim.SetTrigger("RightPunch");
+    }
+
+    public void IsBlocking(InputAction.CallbackContext value)
+    {
+        _anim.SetBool("IsBlocking", value.ReadValueAsButton());
+    }
+    
     private void Animazione()
     {
         movimentoavanti = (_inputMovement.x > 0.1f) ? true: false;

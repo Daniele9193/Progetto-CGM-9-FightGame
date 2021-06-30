23b6ca92bfc26ad23fb1c49353408e68a99db204
Fabio-Morganti <fabiomorga92@gmail.com>
2021-06-30 16:46:55 +0200

    aggiornamento healtbar

--- FightGame/Assets/Scripts/HealthBar/HealthBar.cs
+++ FightGame/Assets/Scripts/HealthBar/HealthBar.cs
@@ -6,22 +6,10 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     
-    public Player player;
     public Slider slider;
     public Gradient gradient;
     public Image fill;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void SetMaxHealth(int health)
     {

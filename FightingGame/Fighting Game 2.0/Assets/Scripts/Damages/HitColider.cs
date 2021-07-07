de045d0c41e291fc515b4805d7302dcb173231e3
Daniele9193 <50204835+Daniele9193@users.noreply.github.com>
2021-07-07 16:17:57 +0200

    Revert "collider"
    This reverts commit f5c854bfc323ade77d7e5df4aa44cf5d1c532ba2.

--- FightingGame/Fighting Game 2.0/Assets/Scripts/Damages/HitColider.cs
+++ FightingGame/Fighting Game 2.0/Assets/Scripts/Damages/HitColider.cs
@@ -12,28 +12,43 @@ public class HitColider : MonoBehaviour
     public Rival rival2;
     public Rival rival3;
     public Rival rival4;
-    //private GameObject[] rivalList;
+    private GameObject[] rivalList;
 
     void Start()
     {
-        //rivalList = new GameObject[transform.childCount];
-        //for (int i = 0; i < transform.childCount; i++)
-            //rivalList[i] = transform.GetChild(i).gameObject;
+        rivalList = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+            rivalList[i] = transform.GetChild(i).gameObject;
     }
     
     public void OnTriggerEnter(Collider other)
     {
+        //Debug.Log(other);
         Rival somebody = other.gameObject.GetComponent<Rival>();
         //GameObject somebody = GameObject.FindWithTag("Rival");
-        
-        if (somebody != null)
+        //Debug.Log("OnTriggerEnter"+ somebody);
+        if (somebody != null && rivalList[0].activeSelf)
         {
             Debug.Log("Hit "+somebody+" Punch");
             rival1.TakeDamage(100);
-            rival2.TakeDamage(100);
-            rival3.TakeDamage(100);
-            rival4.TakeDamage(100);
         }
         
+        if (somebody != null && rivalList[1].activeSelf)
+        {
+            Debug.Log("Hit "+somebody+" Punch");
+            rival2.TakeDamage(100);
+        }
+        
+        if (somebody != null && rivalList[2].activeSelf)
+        {
+            Debug.Log("Hit "+somebody+" Punch");
+            rival3.TakeDamage(100);
+        }
+        
+        if (somebody != null && rivalList[3].activeSelf)
+        {
+            Debug.Log("Hit "+somebody+" Punch");
+            rival4.TakeDamage(100);
+        }
     }
 }

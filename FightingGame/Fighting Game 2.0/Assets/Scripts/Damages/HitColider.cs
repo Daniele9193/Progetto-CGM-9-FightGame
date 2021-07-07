0310d2d42a4171a19541007b4a89541d51e6d8a5
Daniele9193 <50204835+Daniele9193@users.noreply.github.com>
2021-07-07 15:36:54 +0200

    Collider management

--- FightingGame/Fighting Game 2.0/Assets/Scripts/Damages/HitColider.cs
+++ FightingGame/Fighting Game 2.0/Assets/Scripts/Damages/HitColider.cs
@@ -8,7 +8,18 @@ public class HitColider : MonoBehaviour
     public string punchName;
     public float damage;
     public Player owner;
-    public Rival rival;
+    public Rival rival1;
+    public Rival rival2;
+    public Rival rival3;
+    public Rival rival4;
+    private GameObject[] rivalList;
+
+    void Start()
+    {
+        rivalList = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+            rivalList[i] = transform.GetChild(i).gameObject;
+    }
     
     public void OnTriggerEnter(Collider other)
     {
@@ -16,10 +27,28 @@ public class HitColider : MonoBehaviour
         Rival somebody = other.gameObject.GetComponent<Rival>();
         //GameObject somebody = GameObject.FindWithTag("Rival");
         //Debug.Log("OnTriggerEnter"+ somebody);
-        if (somebody != null)
+        if (somebody != null && rivalList[0].activeSelf)
         {
             Debug.Log("Hit "+somebody+" Punch");
-            rival.TakeDamage(100);
+            rival1.TakeDamage(100);
+        }
+        
+        if (somebody != null && rivalList[1].activeSelf)
+        {
+            Debug.Log("Hit "+somebody+" Punch");
+            rival2.TakeDamage(100);
+        }
+        
+        if (somebody != null && rivalList[2].activeSelf)
+        {
+            Debug.Log("Hit "+somebody+" Punch");
+            rival3.TakeDamage(100);
+        }
+        
+        if (somebody != null && rivalList[3].activeSelf)
+        {
+            Debug.Log("Hit "+somebody+" Punch");
+            rival4.TakeDamage(100);
         }
     }
 }
